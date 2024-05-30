bc155a734896ea569a36d3979377f35d1d3e9ea1
tiaede00 <workmail.edem@gmail.com>
2024-05-29 23:45:58 -0400

    - adds mapped paths

--- jupyter_miircic_novnc_proxy/__init__.py
+++ jupyter_miircic_novnc_proxy/__init__.py
@@ -24,6 +24,30 @@ def get_icon_path():
     )
 
 
+def _novnc_urlparams():
+    url_params = """?video_quality=2
+    &enable_webp=false
+    &autoconnect=1
+    &path=miircic_vnc/websockify
+    &cursor=true
+    &resize=remote
+    &clipboard_up=true
+    &clipboard_down=true
+    &clipboard_seamless=true
+    &toggle_control_panel=null"""
+
+    return url_params
+
+
+def _novnc_mappath(path):
+    # always pass the url parameter
+    if path in ('/', '/vnc.html',):
+        url_params = _novnc_urlparams()
+        path = '/vnc.html' + url_params
+
+    return path
+
+
 def setup_vnc_server():
     def _get_env(port):
         return dict(NO_VNC_PORT=str(port))
@@ -35,15 +59,20 @@ def setup_vnc_server():
 
         return cmd
 
+    path_info = 'miircic_vnc/vnc.html' + _novnc_urlparams()
+
     server_process = {
         'command': _get_cmd,
         'timeout': 90,
+        'mappath': _novnc_mappath,
         'absolute_url': False,
         'new_browser_tab': True,
         'environment': _get_env,
         'launcher_entry': {
+            'enabled': True,
             'title': 'MiircicVNC',
-            'icon_path': get_icon_path()
+            'icon_path': get_icon_path(),
+            'path_info': path_info,
         }
     }
     return server_process
