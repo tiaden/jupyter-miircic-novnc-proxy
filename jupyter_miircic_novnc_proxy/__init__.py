5f20c66d9cf419d9117b7a523a20df6a7676456c
tiaede00 <workmail.edem@gmail.com>
2024-07-02 22:08:18 -0400

    - adds README.md

--- jupyter_miircic_novnc_proxy/__init__.py
+++ jupyter_miircic_novnc_proxy/__init__.py
@@ -8,7 +8,7 @@ logger.setLevel('INFO')
 
 def get_vnc_server_script(prog):
     other_paths = [
-        os.path.join('/dockerstartup', prog),
+        os.path.join('/startup', prog),
     ]
     if shutil.which(prog):
         return prog
